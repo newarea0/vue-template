669e156069b067dacc5beccce85d0b884f3874b9
stormzhangbx <13128814051@163.com>
2024-10-23 17:30:11 +0800

    feat: 集成 Pinia

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
+import { useCounterStore } from '../../stores/counter'
 
+const counter = useCounterStore()
 </script>
 
 <template>
-  <div>
-    home
+  <div class="home">
+    <h1>Home</h1>
+    <p>Count: {{ counter.count }}</p>
+    <p>Double Count: {{ counter.doubleCount }}</p>
+    <button @click="counter.increment">
+      Increment
+    </button>
+    <button @click="counter.decrement">
+      Decrement
+    </button>
   </div>
 </template>
 
