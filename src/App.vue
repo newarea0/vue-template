43fad27c6966b308c570f8a37d0d743288feadcf
stormzhangbx <13128814051@163.com>
2024-10-23 16:57:17 +0800

    feat: 集成 ESlint

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,16 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo">
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
-  <p class="text-3xl font-bold underline">hello</p>
+  <p class="text-3xl font-bold underline">
+    hello
+  </p>
 </template>
 
 <style scoped>
